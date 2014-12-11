3026c6afe9a07ba5d8e86a3bea8fc0454da1e9a0
Olegas <reafle@gmail.com>
2014-12-11 13:34:09 +0200

    IdeaVim config file

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ dir=~/dotfiles
 olddir=~/.dotfiles.bak
 
 # list of files/folders to symlink in homedir
-files="bashrc bash_profile bash_aliases vim vimrc"
+files="bashrc bash_profile bash_aliases vim vimrc ideavimrc"
 #installs vundle
 cd ~
 git clone https://github.com/gmarik/Vundle.vim.git $dir/vim/bundle/Vundle.vim
