477ae8776c790006cf6b8a4fb13106b48be86150
reafle <reafle@gmail.com>
2018-06-11 09:44:59 +0300

    Scala; Tasks fixes

--- tmux/linux/tasks.sh
+++ tmux/linux/tasks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 title="t: "
-counter=`python ~/tasks/t/t.py --task-dir ~/tasks --list tasks | wc -l`
+counter=`python ~/.tasks/t/t.py --task-dir ~/.tasks --list tasks | wc -l`
 
 if [ $counter -gt 0 ]; then
     echo $title $counter "|"
